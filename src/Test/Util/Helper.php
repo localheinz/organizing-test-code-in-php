ece93f978cc469acf951a6bd3648201a941a04cf
Andreas Möller <am@localheinz.com>
2023-03-03 09:34:49 +0100

    Enhancement: Declare test code in subdirectories

---
+++ src/Test/Util/Helper.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2024 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/organizing-test-code-in-php
+ */
+
+namespace Localheinz\OrganizingTestCodeInPhp\Test\Util;
+
+use Faker\Factory;
+use Faker\Generator;
+
+/**
+ * @internal
+ */
+trait Helper
+{
+    private static function faker(string $locale = 'en_US'): Generator
+    {
+        /**
+         * @var array<string, Generator>
+         */
+        static $fakers = [];
+
+        if (!\array_key_exists($locale, $fakers)) {
+            $faker = Factory::create($locale);
+
+            $faker->seed(9001);
+
+            $fakers[$locale] = $faker;
+        }
+
+        return $fakers[$locale];
+    }
+}
